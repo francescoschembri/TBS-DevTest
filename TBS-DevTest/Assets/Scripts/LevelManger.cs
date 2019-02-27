6dd3d7aa3e019a01d8e455c8ebf86e5f9734dcd5
francescoschembri <38549608+francescoschembri@users.noreply.github.com>
2019-02-27 16:54:10 +0100

    Added Main Menu

---
+++ TBS-DevTest/Assets/Scripts/LevelManger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManger : MonoBehaviour
+{
+    public void NewGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
